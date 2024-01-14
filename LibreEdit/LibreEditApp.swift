541737c71ec220d276f0be7bf1092fb879f1b231
Christian Cleberg <hello@cleberg.net>
2024-01-13 20:34:00 -0600

    Initial Commit

---
+++ LibreEdit/LibreEditApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreEditApp.swift
+//  LibreEdit
+//
+//  Created by Christian Cleberg on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct LibreEditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
